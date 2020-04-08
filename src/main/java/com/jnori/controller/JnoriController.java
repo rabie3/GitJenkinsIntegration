3ee0c3565170986e44c6647c9b080a0f7ad9b554
jalnor <norrisj0612@gmail.com>
2020-04-08 10:00:12 -0400

    First Commit

---
+++ src/main/java/com/jnori/controller/JnoriController.java
@@ -0,0 +1,13 @@
+package com.jnori.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JnoriController {
+
+	@GetMapping("/")
+	public String getMessage() {
+		return "Welcome to GitJenkins Integration!";
+	}
+}
---
+++ src/test/java/com/jnori/GitJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
